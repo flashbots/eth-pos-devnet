4a7ad791d9d35cb4dd3d05960da6550582324dae
avalonche <avalonche@protonmail.com>
2022-12-14 10:37:38 -0800

    config updates

--- start-local-net.sh
+++ start-local-net.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ ! -d "consensus/prysm" ]; then
     git clone https://github.com/flashbots/prysm.git consensus/prysm
     docker build -t eth-pos-devnet_beacon-chain:latest -f consensus/Dockerfile consensus/prysm
@@ -5,12 +7,15 @@ fi
 
 if [ ! -d "execution/builder" ]; then
     git clone https://github.com/flashbots/builder.git execution/builder
+    cd execution/builder
+    git checkout 1f2047d7894d01c1526673c7ab33873fcae78abd
+    cd ../..
     docker build -t eth-pos-devnet_geth:latest execution/builder
 fi
 
 running="$(docker-compose ps --services --filter "status=running")"
 if [ -n "$running" ]; then
-    docker-compose down
+    docker-compose down --remove-orphans
     rm -rf execution/geth
     rm -rf consensus/beacondata
     rm -rf consensus/validatordata
